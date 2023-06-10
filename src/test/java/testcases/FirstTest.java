caec3955724ae3ec8f4575571f5008d2bb099417
Oleksandra743 <olesya_pl@ukr.net>
2023-06-10 04:37:50 +0300

    new

--- src/test/java/testcases/FirstTest.java
+++ src/test/java/testcases/FirstTest.java
@@ -1,6 +1,7 @@
 package testcases;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class FirstTest {
@@ -10,5 +11,13 @@ public class FirstTest {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\opylypiv\\IdeaProjects\\TestAutomationFramework\\src\\test\\resources\\executables\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://mvnrepository.com/");
+    }
+    @Test
+    public void startDriver(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\opylypiv\\Desktop\\TestFrameworkAuto\\src\\test\\resources\\executables\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver=new ChromeDriver(options);
+        driver.get("https://mvnrepository.com/artifact/org.testng/testng");
     }
 }
