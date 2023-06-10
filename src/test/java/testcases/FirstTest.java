730f85ff49e38b5d62fd9da780101bd240d08f5a
Oleksandra743 <olesya_pl@ukr.net>
2023-06-10 03:06:00 +0300

    first commit

---
+++ src/test/java/testcases/FirstTest.java
@@ -0,0 +1,16 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void openBrowser(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\opylypiv\\IdeaProjects\\TestAutomationFramework\\src\\test\\resources\\executables\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://mvnrepository.com/");
+        driver.manage().window().maximize();
+        driver.close();
+    }
+}
