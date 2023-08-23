965b5ad3b4ab0f26c7290b36cfa69144da04cc25
Oleksandra743 <olesya_pl@ukr.net>
2023-08-23 21:28:38 +0300

    Home_work_19b

--- src/main/java/pageobjects/HomePage.java
+++ src/main/java/pageobjects/HomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 public class HomePage extends BaseMain {
@@ -15,6 +16,13 @@ public class HomePage extends BaseMain {
 
     //Locators
     public String webSiteURL = "http://test.my-fork.com";
+    public String Amazon= "https://www.amazon.com/";
+    public String YouTube= "https://www.youtube.com/";
+    public String Prephouse= "https://prephouse.thinkific.com/";
+    public String wikipedia= "https://en.wikipedia.org/wiki/Main_Page";
+    public String zo= "https://zoskinhealth.com/";
+    public String qagroup= "https://qagroup.com.ua/";
+
 
     public String bankOfAmerica = "https://secure.bankofamerica.com/secure-mycommunications/public/appointments/?marketingCode=NEWHP_ECHMPG";
     public String signInButton = "//div[@class='home-menu-block']//div[@class='menu']//a[2]";
@@ -36,6 +44,24 @@ public class HomePage extends BaseMain {
     public void openWebSite(){
         driver.get(webSiteURL);
     }
+    public void openAmazon(){
+        driver.get(Amazon);
+    }
+    public void openYoutube(){
+        driver.get(YouTube);
+    }
+    public void openPrephouse(){
+        driver.get(Prephouse);
+    }
+    public void openWikipedia(){
+        driver.get(wikipedia);
+    }
+    public void openZo(){
+        driver.get(zo);
+    }
+    public void openQaGroup(){
+        driver.get(qagroup);
+    }
     public  String siteName() {
         log.info("WebSite "+ webSiteURL+ " is opened successfully");
         return null;
@@ -51,5 +77,10 @@ public class HomePage extends BaseMain {
     }
     public void clickOnCourseGalleryButton() {
         driver.findElement(By.xpath("//a[@href='/quizzes-list']")).click();
+    }
+
+
+    public List<Integer> urlVerification() {
+        return verifyLinkActive();
     }
 }
